4214b13fdb82b313a4b736c0bc5cfbffeb9511f0
Om Prakash Pandey <pandeyop@gmail.com>
2016-02-29 11:11:49 +0530

    P2: Tournament Results - Udacity Submission Python module, sql file and tests file for the requirements towards project #2 of Full Stack Nanodegree

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -13,14 +13,36 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    query = "DELETE FROM matches"
+    c.execute(query)
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    query = "DELETE FROM players"
+    c.execute(query)
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    query = "SELECT COUNT(id) AS num FROM players"
+    c.execute(query)
+    results = c.fetchone()
+    conn.close()
+    if results:
+        return results[0]
+    else:
+        return "0"
 
 
 def registerPlayer(name):
@@ -32,6 +54,12 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    c = conn.cursor()
+    query = "INSERT INTO players(name) VALUES(%s)"
+    c.execute(query, (name,))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,6 +75,17 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    query = "SELECT players.id, players.name, \
+             win_count.wins AS wins, total_count.total AS matches \
+             FROM players LEFT JOIN win_count ON players.id = win_count.id \
+             LEFT JOIN total_count ON players.id = total_count.id \
+             ORDER BY wins DESC"
+    c.execute(query)
+    results = c.fetchall()
+    conn.close()    
+    return results
 
 
 def reportMatch(winner, loser):
@@ -56,6 +95,12 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    conn = connect()
+    c = conn.cursor()
+    query = "INSERT INTO matches(winner, loser) VALUES(%s, %s)"
+    c.execute(query, (winner, loser,))
+    conn.commit()
+    conn.close()
  
  
 def swissPairings():
@@ -73,5 +118,14 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
+    conn = connect()
+    c = conn.cursor()
+    query = "SELECT odd_ranks.id AS id1, odd_ranks.name AS name1, \
+             even_ranks.id AS id2, even_ranks.name AS name2 \
+             FROM odd_ranks, even_ranks \
+             WHERE odd_ranks.row_number = even_ranks.row_number"
+    c.execute(query)
+    results = c.fetchall()
+    conn.close()    
+    return results    
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,52 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Create database for tournament.
+CREATE DATABASE tournament;
 
+-- Connect to tournament database.
+\c tournament;
+
+-- Create table for players with serial id and text name.
+CREATE TABLE players ( id SERIAL PRIMARY KEY, name TEXT);
+
+-- Create table for matches with serial id, winner id, and loser id.
+CREATE TABLE matches ( id SERIAL PRIMARY KEY,
+                       winner INTEGER REFERENCES players (id),
+                       loser INTEGER REFERENCES players (id));
+
+-- Create view for determining win count per player and their rank order
+-- sorted in descending order by wins.
+CREATE VIEW win_count as SELECT players.id, players.name,
+                          count(matches.winner) AS wins,
+                          row_number() OVER(ORDER BY count(matches.winner) DESC)
+                         FROM players
+                         LEFT JOIN matches
+                         ON players.id = matches.winner
+                         GROUP BY players.id
+                         ORDER BY wins DESC;
+
+-- Create view for determining total matches played per player sorted in
+-- descending order by total matches played.
+CREATE VIEW total_count as SELECT players.id, players.name,
+                            count(matches.winner + matches.loser) AS total
+                           FROM players
+                           LEFT JOIN matches
+                           ON players.id = matches.winner
+                           OR players.id = matches.loser
+                           GROUP BY players.id
+                           ORDER BY total DESC;
+
+-- Create view for id, name of odd-numbered rankings (non-divisble by 2) to be
+-- paired against corresponding row_number of even-numbered rankings
+CREATE VIEW odd_ranks as SELECT id, name,
+                          row_number() OVER(ORDER BY row_number)
+                         FROM win_count
+                         WHERE row_number%2 != 0;
+
+-- Create view for id, name of even-numbered rankings (divisble by 2) to be
+-- paired against corresponding row_number of odd-numbered rankings 
+CREATE VIEW even_ranks as SELECT id, name,
+                           row_number() OVER(ORDER BY row_number)
+                          FROM win_count
+                          WHERE row_number%2 = 0;
